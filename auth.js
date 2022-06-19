68e471024d957a016c4ccd6dabdaf1580d6427e6
shbnal8 <107800296+shbnal8@users.noreply.github.com>
2022-06-19 16:32:00 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -22,3 +22,29 @@ module.exports = (req, res, next) => {
   req.isAuth = true;
   next();
 };
+
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'I am new!'
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ]
+});
