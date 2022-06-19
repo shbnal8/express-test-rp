596919626b430021d9d9e39ddf90c544556564a3
shbnal8 <107800296+shbnal8@users.noreply.github.com>
2022-06-19 16:43:48 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -48,3 +48,26 @@ const userSchema = new Schema({
     }
   ]
 });
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+  { timestamps: true }
+);
